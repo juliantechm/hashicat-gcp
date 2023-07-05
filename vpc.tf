ba7fb793116610f0ca7a872592b525aa0c008445
Julian Palacio <jp00799349@techmahindra.com>
2023-07-05 20:15:53 +0000

    fixing vpc

--- vpc.tf
+++ vpc.tf
@@ -1,34 +1,16 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.4.0"
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "3.4.0"
 
-    project_id   = var.project
-    network_name = "juli-network"
-    routing_mode = "GLOBAL"
+  network_name = "juli-network"
+  project_id = var.project
 
-    subnets = [
-        {
-            subnet_name           = "subnet-01"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.100.20.0/24"
-            subnet_region         = var.region
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.100.30.0/24"
-            subnet_region             = var.region
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-        }
-    ]
-
+  subnets = [
+  {
+    subnet_name   = "juli-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+  # insert the 3 required variables here
 }
