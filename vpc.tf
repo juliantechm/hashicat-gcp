e7876a6e2d5721ddf82b7e00843e42321321b861
Julian Palacio <jp00799349@techmahindra.com>
2023-07-05 19:37:22 +0000

    fixing vpc

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 7.1"
+    version = "~> 3.4"
 
     project_id   = "var.project"
     network_name = "juli-network"
